52a634ed90ca4a72e181a1b8879dc1261d6228b6
CloudManPro <cloudmanpro111@gmail.com>
2025-10-23 09:44:59 -0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#stop_server.sh
 # Para e remove a aplicação do PM2, se ela estiver rodando
 # A primeira linha verifica se o processo existe para evitar erros na primeira implantação
 pm2 describe ssr-app > /dev/null
